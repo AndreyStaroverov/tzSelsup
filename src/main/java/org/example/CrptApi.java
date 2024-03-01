f0604f3dea72e716c25d3b091068ce9b08d28656
Andrey <fanatSai@yandex.ru>
2024-03-02 01:34:02 +0300

    1.1 thread safe

--- src/main/java/org/example/CrptApi.java
+++ src/main/java/org/example/CrptApi.java
@@ -1,126 +1,296 @@
 package org.example;
 
-import com.google.gson.Gson;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class CrptApi {
-    private static final String API_URL = "https://ismp.crpt.ru/api/v3/lk/documents/create?pg=";
-    private static final Gson gson = new Gson();
-    private final AtomicInteger requestCount = new AtomicInteger(0);
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-    private final Semaphore requestSemaphore;
+    private final HttpClient httpClient;
+    private final Gson gson;
+    private final TimeUnit timeUnit;
+    private final int requestLimit;
+    private long lastRequestTime;
+    private int requestCount;
 
     public CrptApi(TimeUnit timeUnit, int requestLimit) {
-        this.requestSemaphore = new Semaphore(requestLimit);
-        scheduler.scheduleAtFixedRate(this::resetRequestCount, 0, 1, timeUnit);
+        this.httpClient = HttpClient.newHttpClient();
+        this.gson = new GsonBuilder().setPrettyPrinting().create();
+        this.timeUnit = timeUnit;
+        this.requestLimit = requestLimit;
+        this.lastRequestTime = System.currentTimeMillis();
+        this.requestCount = 0;
     }
 
-    public void createDocument(Document document, String signature) {
-        try {
-            requestSemaphore.acquire();
-            HttpRequest request = createRequest(document, signature);
-            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-            handleResponse(response);
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            requestSemaphore.release();
+    public synchronized void createDocument(Document document, String signature) throws IOException, InterruptedException {
+        long currentTime = System.currentTimeMillis();
+        long checkTime = currentTime - lastRequestTime;
+        long intervalInMillis = timeUnit.toMillis(1);
+
+        if (checkTime < intervalInMillis && requestCount >= requestLimit) {
+            long sleepTime = intervalInMillis - checkTime;
+            TimeUnit.MILLISECONDS.sleep(sleepTime);
         }
-    }
 
-    private HttpRequest createRequest(Document document, String signature) throws IOException {
-        String requestBody = gson.toJson(document);
-        return HttpRequest.newBuilder()
-                .uri(URI.create(API_URL + document.getProduct_group()))
-                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+        String requestBody = gson.toJson(new DocumentRequest(document, signature));
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://ismp.crpt.ru/api/v3/lk/documents/create"))
+                .timeout(Duration.ofSeconds(30))
                 .header("Content-Type", "application/json")
-                .header("Authorization", "Bearer <ТОКЕН>")
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
                 .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (currentTime - lastRequestTime >= intervalInMillis) {
+            requestCount = 1;
+        } else {
+            requestCount++;
+        }
+        lastRequestTime = System.currentTimeMillis();
     }
 
-    private void handleResponse(HttpResponse<String> response) {
-        int statusCode = response.statusCode();
-        String responseBody = response.body();
-    }
+    static class DocumentRequest {
+        private final Document description;
+        private final String signature;
 
-    private void resetRequestCount() {
-        requestCount.set(0);
-    }
-
-    private static class Document {
-        private String document_format;
-        private String product_document;
-        private String product_group;
-        private String signature;
-        private String type;
-
-        public String getDocument_format() {
-            return document_format;
-        }
-
-        public void setDocument_format(String document_format) {
-            this.document_format = document_format;
-        }
-
-        public String getProduct_group() {
-            return product_group;
-        }
-
-        public void setProduct_group(String product_group) {
-            this.product_group = product_group;
-        }
-
-        public String getSignature() {
-            return signature;
-        }
-
-        public void setSignature(String signature) {
+        public DocumentRequest(Document description, String signature) {
+            this.description = description;
             this.signature = signature;
         }
+    }
 
+    static class Document {
 
-        public Document(String document_format, String product_document, String product_group, String signature, String type) {
-            this.document_format = document_format;
-            this.product_document = product_document;
-            this.product_group = product_group;
-            this.signature = signature;
-            this.type = type;
+        private String description;
+        private String docId;
+        private String docStatus;
+        private String docType;
+        private boolean importRequest;
+        private String ownerInn;
+        private String participantInn;
+        private String producerInn;
+        private String productionDate;
+        private String productionType;
+        private List<Product> products;
+        private String regDate;
+
+        public Document(String description, String docId, String docStatus, String docType, boolean importRequest,
+                        String ownerInn, String participantInn, String producerInn, String productionDate,
+                        String productionType, List<Product> products, String regDate, String regNumber) {
+            this.description = description;
+            this.docId = docId;
+            this.docStatus = docStatus;
+            this.docType = docType;
+            this.importRequest = importRequest;
+            this.ownerInn = ownerInn;
+            this.participantInn = participantInn;
+            this.producerInn = producerInn;
+            this.productionDate = productionDate;
+            this.productionType = productionType;
+            this.products = products;
+            this.regDate = regDate;
+            this.regNumber = regNumber;
         }
 
-        public String getProduct_document() {
-            return product_document;
+        private String regNumber;
+
+        public String getDescription() {
+            return description;
         }
 
-        public void setProduct_document(String product_document) {
-            this.product_document = product_document;
+        public void setDescription(String description) {
+            this.description = description;
         }
 
-        public String getType() {
-            return type;
+        public String getDocId() {
+            return docId;
         }
 
-        public void setType(String type) {
-            this.type = type;
+        public void setDocId(String docId) {
+            this.docId = docId;
+        }
+
+        public String getDocStatus() {
+            return docStatus;
+        }
+
+        public void setDocStatus(String docStatus) {
+            this.docStatus = docStatus;
+        }
+
+        public String getDocType() {
+            return docType;
+        }
+
+        public void setDocType(String docType) {
+            this.docType = docType;
+        }
+
+        public boolean isImportRequest() {
+            return importRequest;
+        }
+
+        public void setImportRequest(boolean importRequest) {
+            this.importRequest = importRequest;
+        }
+
+        public String getOwnerInn() {
+            return ownerInn;
+        }
+
+        public void setOwnerInn(String ownerInn) {
+            this.ownerInn = ownerInn;
+        }
+
+        public String getParticipantInn() {
+            return participantInn;
+        }
+
+        public void setParticipantInn(String participantInn) {
+            this.participantInn = participantInn;
+        }
+
+        public String getProducerInn() {
+            return producerInn;
+        }
+
+        public void setProducerInn(String producerInn) {
+            this.producerInn = producerInn;
+        }
+
+        public String getProductionDate() {
+            return productionDate;
+        }
+
+        public void setProductionDate(String productionDate) {
+            this.productionDate = productionDate;
+        }
+
+        public String getProductionType() {
+            return productionType;
+        }
+
+        public void setProductionType(String productionType) {
+            this.productionType = productionType;
+        }
+
+        public List<Product> getProducts() {
+            return products;
+        }
+
+        public void setProducts(List<Product> products) {
+            this.products = products;
+        }
+
+        public String getRegDate() {
+            return regDate;
+        }
+
+        public void setRegDate(String regDate) {
+            this.regDate = regDate;
+        }
+
+        public String getRegNumber() {
+            return regNumber;
+        }
+
+        public void setRegNumber(String regNumber) {
+            this.regNumber = regNumber;
         }
     }
 
-    public static void main(String[] args) {
-        CrptApi crptApi = new CrptApi(TimeUnit.SECONDS, 10);
-        String signature = "<Открепленная подпись в base64>";
-        Document document = new Document(
-                "MANUAL",
-                "<Документ в base64>",
-                "group",
-                signature,
-                "LP_INTRODUCE_GOODS"
-        );
-        crptApi.createDocument(document, signature);
+    static class Product {
+
+        private String certificateDocument;
+        private String certificateDocumentDate;
+        private String certificateDocumentNumber;
+        private String ownerInn;
+        private String producerInn;
+        private String productionDate;
+        private String tnvedCode;
+        private String uitCode;
+        private String uituCode;
+
+        public String getCertificateDocument() {
+            return certificateDocument;
+        }
+
+        public void setCertificateDocument(String certificateDocument) {
+            this.certificateDocument = certificateDocument;
+        }
+
+        public String getCertificateDocumentDate() {
+            return certificateDocumentDate;
+        }
+
+        public void setCertificateDocumentDate(String certificateDocumentDate) {
+            this.certificateDocumentDate = certificateDocumentDate;
+        }
+
+        public String getCertificateDocumentNumber() {
+            return certificateDocumentNumber;
+        }
+
+        public void setCertificateDocumentNumber(String certificateDocumentNumber) {
+            this.certificateDocumentNumber = certificateDocumentNumber;
+        }
+
+        public String getOwnerInn() {
+            return ownerInn;
+        }
+
+        public void setOwnerInn(String ownerInn) {
+            this.ownerInn = ownerInn;
+        }
+
+        public String getProducerInn() {
+            return producerInn;
+        }
+
+        public void setProducerInn(String producerInn) {
+            this.producerInn = producerInn;
+        }
+
+        public String getProductionDate() {
+            return productionDate;
+        }
+
+        public void setProductionDate(String productionDate) {
+            this.productionDate = productionDate;
+        }
+
+        public String getTnvedCode() {
+            return tnvedCode;
+        }
+
+        public void setTnvedCode(String tnvedCode) {
+            this.tnvedCode = tnvedCode;
+        }
+
+        public String getUitCode() {
+            return uitCode;
+        }
+
+        public void setUitCode(String uitCode) {
+            this.uitCode = uitCode;
+        }
+
+        public String getUituCode() {
+            return uituCode;
+        }
+
+        public void setUituCode(String uituCode) {
+            this.uituCode = uituCode;
+        }
     }
 }
+
